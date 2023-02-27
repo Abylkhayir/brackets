14ca5842df1a692b913abb189c4ee0a9648e4052
Abylkhayir <88201428+Abylkhayir@users.noreply.github.com>
2023-02-28 00:32:31 +0600

    feat:update this code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+"use strict"
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let pairs = bracketsConfig.map(pair => pair.join(""));
+  while (str.length > 0) {
+    pairs.forEach(pair => str = str.replaceAll(pair, ""));
+    if (
+      str.length > 0 &&
+      !pairs.reduce((a, pair) => str.includes(pair) ? a += 1 : a, 0)
+    ) {
+      return false;
+    }
+  }
+  return true;
 }
